ab7e3194ac940da96aeb96299608b474f3ed018e
Kayvan Mazaheri <KayvanMazaheri@gMail.com>
2016-11-27 10:40:02 +0330

    Add axolla.sh

---
+++ axolla.sh
@@ -0,0 +1,23 @@
+DELAY=20
+LOGFILE=log.txt
+AXEL_PARAMS=$@
+DIFF_PARAMS="-q"
+
+for (( ; ; ))
+do
+	axel $AXEL_PARAMS >> $LOGFILE &
+	PID=$!
+	for (( ; ; ))
+	do
+		cp -T -f $LOGFILE $LOGFILE.OLD
+		sleep $DELAY
+		if [[ diff $DIFF_PARAMS $LOGFILE $LOGFILE.OLD | wc -l  == 0 ]]; then
+			break
+		fi
+	done
+	if [[ ps | grep $PID | wc -l == 0 ]]; then
+		break
+	else
+		kill $PID
+	fi
+done
