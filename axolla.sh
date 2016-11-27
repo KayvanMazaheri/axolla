8cff2237eb44bae9e401f7068e918939dd6eff48
Kayvan Mazaheri <KayvanMazaheri@gMail.com>
2016-11-27 20:24:49 +0330

    cleaning and commenting

--- axolla.sh
+++ axolla.sh
@@ -1,24 +1,26 @@
-# set -x
-DELAY=10
-LOGFILE=$(mktemp -t "axolla.XXXXXXXXXX")
-AXEL_PARAMS=$@
-DIFF_PARAMS="-q"
+# set -x 													# uncomment this line to enable debug mode
 
-while :
+DELAY=10												# refresh time in seconds
+LOGFILE=$(mktemp -t "axolla.XXXXXXXXXX")				# create a temp file to redirect axel logs to it
+AXEL_PARAMS=$@											# save all passed parameter to give them to axel later
+
+while :													# 
 do
-	axel $AXEL_PARAMS > $LOGFILE &
-	PID=$!
-	while :
+	axel $AXEL_PARAMS > $LOGFILE &						# run axel with saved patameters and redirect stdout into the temp file
+	PID=$!												# save the pid of the axel instance process
+	while :												#
 	do
-		> $LOGFILE
-		sleep $DELAY
-		if [[ $(cat $LOGFILE | wc -c)  = 0 ]]; then
-			break
+		> $LOGFILE										# clear the temp file
+		sleep $DELAY									# wait a little for axel to write to the file again
+		if [[ $(cat $LOGFILE | wc -c)  = 0 ]]; then		# check if the file is still empty
+			break										# file was empty, break
 		fi
 	done
-	if [[ $(ps | grep $PID | wc -l) = 0 ]]; then
-		break
+	if [[ $(ps | grep $PID | wc -l) = 0 ]]; then		# check if the axel process is still up
+		break											# process is not running, we are done
 	else
-		kill $PID
+		kill $PID 										# process was stucked, kill it, so we can run it again
 	fi
 done
+
+rm -f $LOGFILE											# remove the temp file
