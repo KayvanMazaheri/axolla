51d45814991d8bd5b2f3ce56243f57e020b7e900
Kayvan Mazaheri <KayvanMazaheri@gMail.com>
2016-11-27 20:29:08 +0330

    use spaces instead of tabs

--- axolla.sh
+++ axolla.sh
@@ -1,26 +1,26 @@
-# set -x 													# uncomment this line to enable debug mode
+# set -x                                                # uncomment this line to enable debug mode
 
-DELAY=10												# refresh time in seconds
-LOGFILE=$(mktemp -t "axolla.XXXXXXXXXX")				# create a temp file to redirect axel logs to it
-AXEL_PARAMS=$@											# save all passed parameter to give them to axel later
+DELAY=10                                                # refresh time in seconds
+LOGFILE=$(mktemp -t "axolla.XXXXXXXXXX")                # create a temp file to redirect axel logs to it
+AXEL_PARAMS=$@                                          # save all passed parameter to give them to axel later
 
-while :													# 
+while :                                                 # 
 do
-	axel $AXEL_PARAMS > $LOGFILE &						# run axel with saved patameters and redirect stdout into the temp file
-	PID=$!												# save the pid of the axel instance process
-	while :												#
-	do
-		> $LOGFILE										# clear the temp file
-		sleep $DELAY									# wait a little for axel to write to the file again
-		if [[ $(cat $LOGFILE | wc -c)  = 0 ]]; then		# check if the file is still empty
-			break										# file was empty, break
-		fi
-	done
-	if [[ $(ps | grep $PID | wc -l) = 0 ]]; then		# check if the axel process is still up
-		break											# process is not running, we are done
-	else
-		kill $PID 										# process was stucked, kill it, so we can run it again
-	fi
+    axel $AXEL_PARAMS > $LOGFILE &                      # run axel with saved patameters and redirect stdout into the temp file
+    PID=$!                                              # save the pid of the axel instance process
+    while :                                             #
+    do
+        > $LOGFILE                                      # clear the temp file
+        sleep $DELAY                                    # wait a little for axel to write to the file again
+        if [[ $(cat $LOGFILE | wc -c)  = 0 ]]; then     # check if the file is still empty
+            break                                       # file was empty, break
+        fi
+    done
+    if [[ $(ps | grep $PID | wc -l) = 0 ]]; then        # check if the axel process is still up
+        break                                           # process is not running, we are done
+    else
+        kill $PID                                       # process was stucked, kill it, so we can run it again
+    fi
 done
 
-rm -f $LOGFILE											# remove the temp file
+rm -f $LOGFILE                                          # remove the temp file
