be6d3589454065f1a132184b8a82dc0b76382cf8
Kayvan Mazaheri <KayvanMazaheri@gMail.com>
2016-11-27 11:40:22 +0330

    Add axolla.sh

--- axolla.sh
+++ axolla.sh
@@ -1,4 +1,5 @@
-DELAY=20
+# set -x
+DELAY=10
 LOGFILE=log.txt
 AXEL_PARAMS=$@
 DIFF_PARAMS="-q"
@@ -11,11 +12,11 @@ do
 	do
 		cp -T -f $LOGFILE $LOGFILE.OLD
 		sleep $DELAY
-		if [[ diff $DIFF_PARAMS $LOGFILE $LOGFILE.OLD | wc -l  == 0 ]]; then
+		if [[ $(diff $DIFF_PARAMS $LOGFILE $LOGFILE.OLD | wc -l)  = 0 ]]; then
 			break
 		fi
 	done
-	if [[ ps | grep $PID | wc -l == 0 ]]; then
+	if [[ $(ps | grep $PID | wc -l) = 0 ]]; then
 		break
 	else
 		kill $PID
